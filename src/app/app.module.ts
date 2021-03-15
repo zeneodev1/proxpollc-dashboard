2ea1414e54453250cfcf8975dab6fd6fbe509ad1
Mohamed <zeneodev1@gmail.com>
2021-03-15 11:40:44 +0100

    product form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
